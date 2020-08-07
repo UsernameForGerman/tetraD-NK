61a9e9c1d98ddcf6a2080bdc684381da1e827fc4
Whatruska <vitdub@inbox.ru>
2020-08-07 18:54:49 +0600

    [Front] content-type fix

--- landing/src/fetchCases.js
+++ landing/src/fetchCases.js
@@ -4,7 +4,8 @@ import {CMS_URL} from "./getBaseUrl";
 export default function fetchCases(setFetching, setCases) {
     setFetching(true);
     axios.create({
-        baseURL: CMS_URL
+        baseURL: CMS_URL,
+        Content-Type: "application/json"
     }).get('cases').then(resp => {
         setCases(resp.data)
     })
