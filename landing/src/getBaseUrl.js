124680d779f94e51da8d621d756768ffd363df25
German <polyakgerman@gmail.com>
2020-08-07 12:56:37 +0300

    [Deploy] - add deploy production settings

--- landing/src/getBaseUrl.js
+++ landing/src/getBaseUrl.js
@@ -1,5 +1,5 @@
 
-//const CMS_URL = 'http://cms.tetradnk.ru/'
-const CMS_URL = 'http://localhost:9000/'
+const CMS_URL = 'http://cms.tetradnk.ru/'
+// const CMS_URL = 'http://localhost:9000/'
 //const CMS_URL = process.env.REACT_APP_CMS_URL
 export {CMS_URL}
--- tetrad_cms/config/production.py
+++ tetrad_cms/config/production.py
@@ -14,3 +14,20 @@ DATABASES = {
         'PORT': os.environ.get('POSTGRES_PORT'),
     }
 }
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = '/var/tetrad/media/'
+STATIC_ROOT = '/var/tetrad/static/'
+
+# MEDIA_ROOT = os.path.join(DATA_DIR, 'media')
+# STATIC_ROOT = os.path.join(DATA_DIR, 'static')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'core', 'static'),
+)
+SITE_ID = 1
