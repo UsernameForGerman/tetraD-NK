ad9993d0513923b058429445e0e6593684263349
German <polyakgerman@gmail.com>
2020-08-09 01:45:53 +0300

    [CMS] - celery task telegram bot api url import fix

--- tetrad_cms/cases/tasks.py
+++ tetrad_cms/cases/tasks.py
@@ -4,9 +4,20 @@ from requests import Session
 
 from core.celery import app
 
+TELEGRAM_BOT_API_URL='http://telegram:5000/'
+
+if settings.TELEGRAM_BOT_API_URL:
+    print('Env telegram bot api url')
+    tg_bot_api_url = settings.TELEGRAM_BOT_API_URL
+else:
+    print('Ooops!')
+    tg_bot_api_url = TELEGRAM_BOT_API_URL
+
+
+
 @app.task(queue='cms')
 def send_new_contact_to_admins(contact: dict, admins: list) -> None:
     s = Session()
     print('sending from cms {} {}'.format(admins, contact))
-    s.post(settings.TELEGRAM_BOT_API_URL + 'send/contact', data={'admins': admins, 'contact': contact})
+    s.post(tg_bot_api_url + 'send/contact', data={'admins': admins, 'contact': contact})
 
