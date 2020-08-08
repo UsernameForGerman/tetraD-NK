458498bca393c48e2ef3cc89abd088d6847d440f
German <polyakgerman@gmail.com>
2020-08-09 01:29:48 +0300

    [CMS] - admin register Admin model

--- tetrad_cms/cases/admin.py
+++ tetrad_cms/cases/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import CasePluginModel, Case, Contact, Task
+from .models import CasePluginModel, Case, Contact, Task, Admin
 # Register your models here.
 
 admin.site.register(CasePluginModel)
 admin.site.register(Case)
 admin.site.register(Contact)
 admin.site.register(Task)
+admin.site.register(Admin)
