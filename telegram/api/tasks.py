66ac09e08eec751b800f2affff24a91b450c0105
German <polyakgerman@gmail.com>
2020-08-09 02:22:25 +0300

    [CMS & Telegram] - fix env variables getting

--- telegram/api/tasks.py
+++ telegram/api/tasks.py
@@ -8,9 +8,11 @@ from core.celery import app
 @app.task(queue='telegram')
 def send_message(chat_id, text='') -> None:
     s = Session()
-    telegram_url = os.environ.get('TELEGRAM_URL')
-    telegram_token = os.environ.get('TELEGRAM_TOKEN')
-    url = '{url}/bot{token}/sendMessage'.format(url=telegram_url, token=telegram_token)
+    # telegram_url = os.environ.get('TELEGRAM_URL')
+    # telegram_token = os.environ.get('TELEGRAM_TOKEN')
+    # url = '{url}/bot{token}/sendMessage'.format(url=telegram_url, token=telegram_token)
+    url = 'https://api.telegram.org/bot1154390364:AAETocdNSsgmW5RJ5pu_U45_wJr76m6r4vQ/sendMessage'
+
     print('send telegram cms {} {}'.format(chat_id, text))
     s.post(url, data={'chat_id': chat_id, 'text': text})
 
--- tetrad_cms/cases/tasks.py
+++ tetrad_cms/cases/tasks.py
@@ -11,6 +11,6 @@ def send_new_contact_to_admins(contact: dict, admins: list) -> None:
     s = Session()
     print('sending from cms {} {}'.format(admins, contact))
     data = {'admins': admins, 'contact': contact}
-    url = os.environ.get('TELEGRAM_BOT_API_URL') + 'send/contact'
+    url = 'http://telegram:5000/send/contact'
     s.post(url, data=dumps(data), headers={'Content-Type': 'application/json'})
 
